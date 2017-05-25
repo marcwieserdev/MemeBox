784b1e4be13dcbe1a384cf74d4061adb45279ded
marc wieser <marc.wieser@epitech.eu>
2017-05-25 17:49:55 +0200

    And now with assets

--- app/src/main/java/com/marc_wieser/memesbox/MainActivity.kt
+++ app/src/main/java/com/marc_wieser/memesbox/MainActivity.kt
@@ -1,16 +1,17 @@
 package com.marc_wieser.memesbox
 
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
+import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
-import android.view.MenuItem
-import android.widget.TextView
+import com.marc_wieser.memesbox.Adapters.PagerAdapter
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val pager : ViewPager = findViewById(R.id.pager) as ViewPager
+        pager.adapter = PagerAdapter(supportFragmentManager, this)
     }
 
 }
